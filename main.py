5104529b63f26cced68cda0cbc90d08cff1a4847
crowbar_sk <sochor.filip@gmail.com>
2024-11-13 19:40:47 +0100

    First content

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+import os
+from typing import Literal, Optional
+from uuid import uuid4
+from fastapi import FastAPI, HTTPException
+import random
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from mangum import Mangum
+
+@app.get("/")
+async def root():
+    return {"message": "Filip's site"}
