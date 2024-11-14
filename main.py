a980249d5d8c10b7594a713a69193b75a8e0c291
crowbar_sk <sochor.filip@gmail.com>
2024-11-14 16:10:10 +0100

    Add database connection

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import random
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from mangum import Mangum
+import psycopg2
+
 
 app = FastAPI()
 handler = Mangum(app)
 counter = 0
+connection = psycopg2.connect(database="postgres", user="postgres", password="Salamandra89+", host="postgres.ctksisewojwq.eu-north-1.rds.amazonaws.com", port=5432)
+cursor = connection.cursor()
 messages_list = ["Filip's site","Awesome AWS","Posunovanie"] 
 
 @app.get("/")
@@ -19,3 +23,10 @@ async def root():
     counter += 1
     message_val = counter % 3
     return {"message": messages_list[message_val]}
+
+@app.get("/trains")
+async def root():
+    global cursor
+    cursor.execute("SELECT train_name, train_value from train_properties;")
+    record = cursor.fetchall()
+    return {"Fetched data", record}
